8ff5a0d46d4ce81d48b98f4d835ec3fc7d717669
Piotr Lecicki <p.lecicki@gmail.com>
2023-11-29 17:26:25 +0100

    Create Spring Cloud Config Server

---
+++ spring-cloud-config-server/src/test/java/p/plecicki/microservices/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package p.plecicki.microservices.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
